3bc707f4ac16c621a577e64bf3e39606e4671fcc
ACSL-Lab04-02 <ACSL-Lab04-02@DESKTOP-PL26BAJ>
2019-12-24 18:01:47 +0600

    Add project files.

---
+++ classpractice_ex11/ProductService.svc.cs
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using classpractice_ex11.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Data.Services.Providers;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace classpractice_ex11
+{
+    public class ProductService : EntityFrameworkDataService<ProductDbContext>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+    }
+}
